b7713265f1f864072b7d88e204de21904e57c63f
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-05-29 12:58:01 +0900

    修正

--- src/gcd.py
+++ src/gcd.py
@@ -16,3 +16,23 @@ def gcd(a: int, b: int) -> int:
     if a == 0:
         return b
     return gcd(b % a, a)
+
+
+def gcd_loop(a: int, b: int) -> int:
+    """ Euclidの互除法を用いて、aとbの最大公約数を求める。非再帰版。
+
+    Args:
+        a, b (int): 最大公約数を求めたい2つの整数
+
+    Returns:
+        g (int): aとbの最大公約数
+
+    Examples:
+        >>> gcd_loop(15, 20)
+        5
+        >>> gcd_loop(3, 7)
+        1
+    """
+    while b > 0:
+        a, b = b, a % b
+    return a
