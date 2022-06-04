ffda5d58ccf3824dd9e7e48004ca6767041f10ff
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-06-04 16:38:34 +0900

    修正

---
+++ src/fibonacci_sequence_matrix.py
@@ -0,0 +1,32 @@
+from n_times import n_times
+
+
+def fibonacci_sequence_matrix(n: int) -> int:
+    """ Fibonacci数F_nを計算する。(行列計算を用いたバージョン)
+
+    Args:
+        n (int): n番目のFibonacci数を求める
+
+    Returns:
+        int: n番目のFibonacci数。
+             ここで、F_0 = 0, F_1 = 1, F_2 = 1, ... である。
+
+    Examples:
+        >>> fibonacci_sequence_fast(5)
+        5
+        >>> fibonacci_sequence_fast(11)
+        89
+    """
+    def _matrix_mul(a, b):
+        """ 2x2行列の積を計算する
+        """
+        c = [[0, 0], [0, 0]]
+        for i in range(2):
+            for k in range(2):
+                for j in range(2):
+                    c[i][j] += a[i][k] * b[k][j]
+        return c
+    if n <= 1:
+        return n
+    f1 = [[1, 1], [1, 0]]
+    return n_times(f1, n, _matrix_mul)[0][1]
