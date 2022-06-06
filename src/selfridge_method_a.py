d0afcaefff544ee6eb514eb7878ed734de97c9af
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-06-06 19:59:00 +0900

    追加

---
+++ src/selfridge_method_a.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+from jacobi_symbol import jacobi_symbol
+
+
+def selfridge_method_a(n: int) -> Tuple[int, int]:
+    """ SelfridgeのMethod Aを用いて、Lucas擬素数テストの適切なパラメータを得る。
+
+    Args:
+        n (int): 平方数ではない奇数
+
+    Returns:
+        a(int), b(int): Lucas擬素数テストのためのパラメータ。
+                        合成数であることが判明した場合は(0, 0)
+    """
+    delta = 5
+    diff = -2
+    while True:
+        jacobi = jacobi_symbol(delta, n)
+        if jacobi == -1:
+            return 1, (1 - delta) // 4
+        if jacobi == 0:
+            return 0, 0
+        delta = diff - delta
+        diff *= -1
