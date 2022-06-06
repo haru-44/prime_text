2cf46ccb359f3696d6bba93bdf3b67b6a8d7e3d2
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-06-06 19:54:16 +0900

    修正

--- src/jacobi_symbol.py
+++ src/jacobi_symbol.py
@@ -14,10 +14,11 @@ def jacobi_symbol(a: int, m: int) -> int:
         >>> jacobi_symbol(5, 11)
         1
     """
+    a %= m
     if a == 0:
         return 1 if m == 1 else 0
     if a % 2 == 1:
         sign = -1 if a % 4 == m % 4 == 3 else 1
-        return sign * jacobi_symbol(m % a, a)
+        return sign * jacobi_symbol(m, a)
     sign = -1 if m % 8 == 3 or m % 8 == 5 else 1
     return sign * jacobi_symbol(a // 2, m)
