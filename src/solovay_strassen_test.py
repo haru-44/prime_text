4e31f615bf5248a6c393917c3404270165b03799
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-06-10 22:34:03 +0900

    test_solovay_strassen_test.py の追加

--- src/solovay_strassen_test.py
+++ src/solovay_strassen_test.py
@@ -1,29 +1,32 @@
 import random
+from typing import List, Optional
 
 from jacobi_symbol import jacobi_symbol
 from legendre_symbol import legendre_symbol
 
 
-def solovay_strassen_test(n: int, k: int = 10) -> str:
+def solovay_strassen_test(n: int, k: Optional[int] = None, a_list: Optional[List[int]] = None) -> str:
     """ Solovay-Strassenテストを用いて、n>2が素数かを判定する。
 
     Args:
         n (int): 素数判定する対象の奇数
-        k (int): 試行回数。既定値を10としたが、特に意味はない。
-                 合成数だが'probable prime'と判定する確率は高々2^{-k}
+        k (int): 試行回数の指定
+        a_list(List[int]): 底の指定
 
     Returns:
         string: 'composite number' = nは合成数
                 'probable prime'   = nはおそらく素数
 
     Examples:
-        >>> solovay_strassen_test(101)
+        >>> solovay_strassen_test(101, a_list=[2])
         'probable prime'
-        >>> solovay_strassen_test(111)
+        >>> solovay_strassen_test(111, a_list=[2])
         'composite number'
     """
-    for _ in range(k):
-        a = random.randrange(2, n)
-        if jacobi_symbol(a, n) != legendre_symbol(a, n):
+    if a_list is None:
+        a_list = [random.randrange(2, n) for _ in range(k)]
+    for a in a_list:
+        euler_criterion = legendre_symbol(a, n)
+        if euler_criterion == 0 or euler_criterion != jacobi_symbol(a, n):
             return 'composite number'
     return 'probable prime'
---
+++ tests/test_solovay_strassen_test.py
@@ -0,0 +1,40 @@
+from src.solovay_strassen_test import solovay_strassen_test
+from sympy.ntheory import isprime
+
+
+def test_fermat_test_A047713():
+    """
+    Notes:
+        https://oeis.org/A047713
+    """
+    pseudoprimes = [561, 1105, 1729, 1905, 2047, 2465, 3277, 4033,
+                    4681, 6601, 8321, 8481, 10585, 12801, 15841, 16705,
+                    18705]
+
+    for n in range(3, 20000, 2):
+        ans = solovay_strassen_test(n, a_list=[2])
+        if isprime(n):
+            assert ans == 'probable prime'
+        elif n in pseudoprimes:
+            assert ans == 'probable prime'
+        else:
+            assert ans == 'composite number'
+
+
+def test_fermat_test_A048950():
+    """
+    Notes:
+        https://oeis.org/A048950
+    """
+    pseudoprimes = [121, 703, 1729, 1891, 2821, 3281, 7381, 8401,
+                    8911, 10585, 12403, 15457, 15841, 16531, 18721,
+                    19345]
+
+    for n in range(5, 20000, 2):
+        ans = solovay_strassen_test(n, a_list=[3])
+        if isprime(n):
+            assert ans == 'probable prime'
+        elif n in pseudoprimes:
+            assert ans == 'probable prime'
+        else:
+            assert ans == 'composite number'
