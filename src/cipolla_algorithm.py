45e5b027adf1444cc2cbc57a4633feb8c7ce9f23
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-06-22 20:59:29 +0900

    修正

--- src/cipolla_algorithm.py
+++ src/cipolla_algorithm.py
@@ -1,10 +1,12 @@
 import random
 
+from inverse_mod import inverse_mod
 from legendre_symbol import legendre_symbol
+from lucas_sequence_chain import lucas_sequence_chain
 from n_times import n_times
 
 
-def cipolla_algorithm(a:int, p:int) -> int:
+def cipolla_algorithm(a: int, p: int) -> int:
     """ Cipollaのアルゴリズム。
 
     x^2 = a mod p
@@ -35,4 +37,34 @@ def cipolla_algorithm(a:int, p:int) -> int:
             break
 
     x, _ = n_times((t, 1), (p+1)//2, _mul)
+    return min(x, -x % p)
+
+
+def cipolla_algorithm_lucas(a: int, p: int) -> int:
+    """ Lucas数列を用いたCipollaのアルゴリズム
+
+    x^2 = a mod p
+    となるようなxを求める。
+    このようなxが存在する場合、x,-xの2つが解となる。
+
+    Args:
+        a (int): 整数で、pを法として平方剰余
+        p (int): 奇素数
+
+    Returns:
+        x (int): x^2 = a mod pを満たすx。
+
+    Examples:
+        >>> cipolla_algorithm_lucas(2, 7) # 4も解であるが、小さいほうの3を返す
+        3
+    """
+    assert legendre_symbol(a, p) == 1
+    while True:
+        t = random.randrange(0, p-1)
+        s = (t**2 - 4*a) % p
+        if legendre_symbol(s, p) == -1:
+            break
+
+    k, _ = lucas_sequence_chain((p+1)//2, t, a, p)
+    x = (k * inverse_mod(2, p)) % p
     return min(x, -x % p)
--- tests/test_cipolla_algorithm.py
+++ tests/test_cipolla_algorithm.py
@@ -1,4 +1,4 @@
-from src.cipolla_algorithm import cipolla_algorithm
+from src.cipolla_algorithm import cipolla_algorithm, cipolla_algorithm_lucas
 from sympy.ntheory import legendre_symbol
 
 
@@ -7,3 +7,10 @@ def test_cipolla_algorithm_01():
         for a in range(1, p):
             if legendre_symbol(a, p) == 1:
                 assert pow(cipolla_algorithm(a, p), 2, p) == a
+
+
+def test_cipolla_algorithm_lucas_01():
+    for p in [3, 11, 101]:
+        for a in range(1, p):
+            if legendre_symbol(a, p) == 1:
+                assert pow(cipolla_algorithm_lucas(a, p), 2, p) == a
