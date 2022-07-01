26e2dc7cf7f5e268656da15117980607f5097f4b
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-07-01 22:51:36 +0900

    修正

--- src/is_wolstenholme_prime.py
+++ src/is_wolstenholme_prime.py
@@ -13,9 +13,14 @@ def is_wolstenholme_prime(p: int) -> bool:
         >>> is_wolstenholme_prime(11)
         False
     """
-    s = p//6 + 1
-    coef0, coef1 = pow(s, 3, p), 1
-    for k in range(s + 1, p//4 + 1):
-        k3 = pow(k, 3, p)
+    k = p//6 + 1
+    delta1 = (3*k**2 + 3*k + 1) % p
+    delta2 = 12 - (p % 6)
+    k3 = pow(k, 3, p)
+    coef0, coef1 = 1, 0
+    for _ in range(k, p//4 + 1):
         coef0, coef1 = (coef0 * k3) % p, (coef0 + coef1 * k3) % p
+        k3 = (k3 + delta1) % p
+        delta1 = (delta1 + delta2) % p
+        delta2 = (delta2 + 6) % p
     return coef1 == 0
