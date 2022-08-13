1918092adb8055bda8649d6e282506c7d07a7c83
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-08-13 18:34:26 +0900

    修正

--- src/pseudosquare_test.py
+++ src/pseudosquare_test.py
@@ -1,7 +1,7 @@
 from is_perfect_power import is_perfect_power
 
 
-def pseudosquare_test(n):
+def pseudosquare_test(n: int) -> str:
     """ 擬平方数を使って素数判定を行う。
 
     3818929未満について、確定的に素数判定を行う。
@@ -26,24 +26,27 @@ def pseudosquare_test(n):
     Notes:
         擬平方数のテーブルは、以下の文献を参照した。
         Jonathan P. Sorenson
-        "Sieving for Pseudosquares and Pseudocubes in Parallel Using Doubly-Focused Enumeration and Wheel Datastructures"
+        "Sieving for Pseudosquares and Pseudocubes in Parallel Using
+        Doubly-Focused Enumeration and Wheel Datastructures"
         DOI: 10.1007/978-3-642-14518-6_26
     """
     if is_perfect_power(n):
         return 'perfect power'
-    t = pow(2, (n-1) // 2, n)
-    if n % 8 == 5 and t != n - 1:
+    n1 = n - 1
+    n2 = n1 // 2
+    t = pow(2, n2, n)
+    if n % 8 == 5 and t != n1:
         return 'composite number'
-    if 1 < t < n - 1:
+    if 1 < t < n1:
         return 'composite number'
     flg = False
     for p, l_p in [(3, 73), (5, 241), (7, 1009), (11, 2641), (13, 8089),
                    (17, 18001), (19, 53881), (23, 87481), (29, 117049),
                    (31, 515761), (37, 1083289), (41, 3206641), (43, 3818929)]:
-        t = pow(p, (n-1) // 2, n)
-        if 1 < t < n - 1:
+        t = pow(p, n2, n)
+        if 1 < t < n1:
             return 'composite number'
-        if t == n - 1:
+        if t == n1:
             flg = True
         if n < l_p:
             break
