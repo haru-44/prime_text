e221a81776fed842f11479ae2aff86019b5a3b16
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-10-29 10:29:37 +0900

    修正

--- src/proth_test.py
+++ src/proth_test.py
@@ -1,6 +1,7 @@
 import random
 from typing import List, Optional
 
+from euler_criterion import euler_criterion
 from jacobi_symbol import jacobi_symbol
 
 
@@ -29,12 +30,41 @@ def proth_test(s: int, m: int, *,
         'possibly composite'
     """
     n = m * 2**s + 1
+    if m % 2 == 0 or 2**s <= m:
+        raise ValueError()
     if a_list is None:
         a_list = [random.randrange(2, n) for _ in range(k)]
     for a in a_list:
         if jacobi_symbol(a, n) == -1:
-            if pow(a, (n - 1) // 2, n) == n - 1:
+            if euler_criterion(a, n) == -1:
                 return 'prime number'
             else:
                 return 'composite number'
     return 'possibly composite'
+
+
+def proth_test_3(s: int, m: int) -> str:
+    """ Prothテストを用いて、n = m * 2**s + 1が素数かを判定する。
+
+    n > 3 かつ m % 3 != 0 のとき、確定的な判定を与える。
+
+    Args:
+        s (int): m < 2**s を満たす
+        m (int): 奇数で3の倍数ではない
+
+    Returns:
+        string: 'prime number'     = nは素数
+                'composite number' = nは合成数
+
+    Examples:
+        >>> proth_test_3(3, 5) # 5 * 2**3 + 1 = 41 is prime
+        'prime number'
+        >>> proth_test_3(8, 5) # 5 * 2**8 + 1 = 1281 is composite
+        'composite number'
+    """
+    n = m * 2**s + 1
+    if n == 3 or m % 2 == 0 or m % 3 == 0 or 2**s <= m:
+        raise ValueError()
+    if euler_criterion(3, n) == -1:
+        return 'prime number'
+    return 'composite number'
--- tests/test_proth_test.py
+++ tests/test_proth_test.py
@@ -1,5 +1,12 @@
-from src.proth_test import proth_test
+from src.proth_test import *
 from src.split_int import split_int
+
+A080076 = [3, 5, 13, 17, 41, 97, 113, 193, 241, 257, 353,
+           449, 577, 641, 673, 769, 929, 1153, 1217, 1409,
+           1601, 2113, 2689, 2753, 3137, 3329, 3457, 4481,
+           4993, 6529]
+correct = [2, 2, 2, 3, 3, 5, 3, 5, 7, 3, 3, 3, 5, 3, 5, 7, 3,
+           5, 3, 3, 3, 5, 13, 3, 3, 3, 5, 3, 5, 7]
 
 
 def test_proth_test_A248972():
@@ -7,13 +14,18 @@ def test_proth_test_A248972():
     Notes:
         https://oeis.org/A248972
     """
-    A080076 = [3, 5, 13, 17, 41, 97, 113, 193, 241, 257, 353,
-               449, 577, 641, 673, 769, 929, 1153, 1217, 1409,
-               1601, 2113, 2689, 2753, 3137, 3329, 3457, 4481,
-               4993, 6529]
-    correct = [2, 2, 2, 3, 3, 5, 3, 5, 7, 3, 3, 3, 5, 3, 5, 7, 3,
-               5, 3, 3, 3, 5, 13, 3, 3, 3, 5, 3, 5, 7]
-
     for n, a in zip(A080076, correct):
-        s, m = split_int(n-1)
+        s, m = split_int(n - 1)
         assert proth_test(s, m, a_list=[a]) == 'prime number'
+
+
+def test_proth_test_3_A248972():
+    """
+    Notes:
+        https://oeis.org/A248972
+    """
+    for n in A080076:
+        s, m = split_int(n - 1)
+        if n == 3 or m % 3 == 0:
+            continue
+        assert proth_test_3(s, m) == 'prime number'
