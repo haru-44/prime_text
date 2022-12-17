1a480d43c21cb641538940f75fed3394d9b1e477
haru-44 <36563693+haru-44@users.noreply.github.com>
2022-12-17 23:00:43 +0900

    修正

--- src/quadratic_form_composition.py
+++ src/quadratic_form_composition.py
@@ -9,7 +9,7 @@ def quadratic_form_composition(a1: int, b1: int, c1: int, a2: int, b2: int, c2: 
 
     Args:
         a1, b1, c1 (int): 2次形式のパラメータ。
-        a2, b2, c2 (int): 2次形式のパラメータ。2つの2次形式の判別式は一致すること。
+        a2, b2, c2 (int): 2次形式のパラメータ。2つの2次形式の判別式は一致し、負であること。
 
     Returns:
         a3, b3, c3 (int): 2つの2次形式の合成。ただし、簡約形式である。
@@ -21,3 +21,45 @@ def quadratic_form_composition(a1: int, b1: int, c1: int, a2: int, b2: int, c2: 
     b3 = b2 + 2 * a2 * ((b1 - b2) // 2 * v * u - c2 * w) // g
     c3 = (b3**2 - D) // (4 * a3)
     return quadratic_form_reduction(a3, b3, c3)
+
+
+def quadratic_form_composition2(a: int, b: int, c: int) -> Tuple[int, int, int]:
+    """ 2次形式(a,b,c)の2乗を計算する。
+
+    Args:
+        a, b, c (int): 2次形式のパラメータ。判別式は負であること。
+
+    Returns:
+        a_, b_, c_ (int): 2次形式(a,b,c)の2乗。
+    """
+    D = b**2 - 4 * a * c
+    g, _, w = extended_gcd(a, b)
+    k = a // g
+    a_ = k**2
+    b_ = b - 2 * k * c * w
+    c_ = (b_**2 - D) // (4 * a_)
+    return quadratic_form_reduction(a_, b_, c_)
+
+
+def quadratic_form_composition3(a: int, b: int, c: int) -> Tuple[int, int, int]:
+    """ 2次形式(a,b,c)の3乗を計算する。
+
+    Args:
+        a, b, c (int): 2次形式のパラメータ。判別式は負であること。
+
+    Returns:
+        a_, b_, c_ (int): 3次形式(a,b,c)の3乗。
+    """
+    D = b**2 - 4 * a * c
+    g, _, w = extended_gcd(a, b)
+    k = a // g
+    a_ = k**2
+    t = k * c * w
+    h, _, V = extended_gcd(a_, a)
+    g, U, w = extended_gcd(h, b - t)
+    k = a // g
+    a_ = a_ * k // g
+    t = k * (t * V * U + c * w)
+    b_ = b - 2 * t
+    c_ = (b_**2 - D) // (4 * a_)
+    return quadratic_form_reduction(a_, b_, c_)
--- tests/test_quadratic_form_composition.py
+++ tests/test_quadratic_form_composition.py
@@ -1,5 +1,6 @@
 from src.n_times import n_times
-from src.quadratic_form_composition import quadratic_form_composition
+from src.quadratic_form_class_number_naive import *
+from src.quadratic_form_composition import *
 
 
 def test_quadratic_form_composition_d3():
@@ -20,3 +21,29 @@ def test_quadratic_form_composition_d23():
     assert quadratic_form_composition(*f, *g) == (1, 1, 6)
     assert n_times(f, 2, lambda x, y: quadratic_form_composition(*x, *y)) == g
     assert n_times(g, 2, lambda x, y: quadratic_form_composition(*x, *y)) == f
+
+
+def test_quadratic_form_composition2_d3():
+    f = 1, 1, 1
+    assert quadratic_form_composition2(*f) == (1, 1, 1)
+
+
+def test_quadratic_form_composition2_d15():
+    f = 2, 1, 2
+    assert quadratic_form_composition2(*f) == (1, 1, 4)
+    f = 1, 1, 4
+    assert quadratic_form_composition2(*f) == (1, 1, 4)
+
+
+def test_quadratic_form_composition2_001():
+    for D in list(range(-4, -200, -4)) + list(range(-3, -200, -4)):
+        for a, b, c in quadratic_form_class_number_naive(D):
+            assert quadratic_form_composition(
+                a, b, c, a, b, c) == quadratic_form_composition2(a, b, c)
+
+
+def test_quadratic_form_composition3_001():
+    for D in list(range(-4, -200, -4)) + list(range(-3, -200, -4)):
+        for a, b, c in quadratic_form_class_number_naive(D):
+            assert quadratic_form_composition(
+                a, b, c, *quadratic_form_composition2(a, b, c)) == quadratic_form_composition3(a, b, c)
